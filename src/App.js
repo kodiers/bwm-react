6f177caf0243a1ddcc4c1e4da01194486352a099
Victor Yamchinov <kodiers@gmail.com>
2018-11-14 04:22:33 +0300

    added routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 import {Header} from "./shared/Header";
 import {RentalList} from "./components/rental/RentalList";
+import {RentalDetail} from "./components/rental/RentalDetail";
+
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <Header/>
-          <div className='container'>
-              <RentalList/>
+      <BrowserRouter>
+          <div className="App">
+            <Header/>
+            <div className='container'>
+              <Route exact path='/' component={RentalList}/>
+              <Route exact path='/test' component={RentalDetail}/>
+            </div>
           </div>
-      </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/rental/RentalDetail.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class RentalDetail extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>I am rental detail</h1>
+            </div>
+        );
+    }
+}
