6eb6a4a14f5e6f9ff9cd3cce08047d096163e89d
Victor Yamchinov <kodiers@gmail.com>
2018-11-23 03:27:46 +0300

    works on rental-detail componet

--- src/components/rental/rental-detail/RentalDetail.js
+++ src/components/rental/rental-detail/RentalDetail.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 import {connect} from 'react-redux';
 
+import {RentalDetailInfo} from "./RentalDetailInfo";
 import * as actions from '../../../actions';
+import {MapWithAMarker} from "../../map/GoogleMap";
 
 class RentalDetail extends React.Component {
 
@@ -15,9 +17,32 @@ class RentalDetail extends React.Component {
         const rental = this.props.rental;
         if (rental._id) {
             return (
-                <div>
-                    <h1>{rental.title}</h1>
-                </div>
+                <section id='rentalDetails'>
+                    <div className='upper-section'>
+                        <div className='row'>
+                            <div className='col-md-6'>
+                                <img src={rental.image} alt=''></img>
+                            </div>
+                            <div className='col-md-6'>
+                                <MapWithAMarker
+                                    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAaJChbXH2CRAG_YeKfmrrJ9kZzpNNgrKg&v=3.exp&libraries=geometry,drawing,places"
+                                    loadingElement={<div style={{ height: `100%` }} />}
+                                    containerElement={<div style={{ height: `360px` }} />}
+                                    mapElement={<div style={{ height: `100%` }} />}
+                                />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='details-section'>
+                        <div className='row'>
+                            <div className='col-md-8'>
+                                <RentalDetailInfo rental={rental}/>
+                            </div>
+                            <div className='col-md-4'> BOOKING</div>
+                        </div>
+                    </div>
+                </section>
             );
         }
         return (
