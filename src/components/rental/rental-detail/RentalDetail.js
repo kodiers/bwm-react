c3e82718f58bf18c7d902a49f5e9e1b8cd7aaf88
Victor Yamchinov <kodiers@gmail.com>
2019-01-15 04:08:48 +0300

    start working on rental-update

--- src/components/rental/rental-detail/RentalDetail.js
+++ src/components/rental/rental-detail/RentalDetail.js
@@ -3,6 +3,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 
 import {RentalDetailInfo} from "./RentalDetailInfo";
+import {RentalDetailUpdate} from "./RentalDetailUpdate";
 import * as actions from '../../../actions';
 import {RentalMap} from "./RentalMap";
 import Booking from "../../booking/Booking";
@@ -12,6 +13,11 @@ class RentalDetail extends React.Component {
     componentWillMount() {
         const rentalId = this.props.match.params.id;
         this.props.dispatch(actions.fetchRentalById(rentalId));
+    }
+
+    renderRentalDetail(rental) {
+        const {isUpdate} = this.props.location.state || false;
+        return isUpdate ? <RentalDetailUpdate rental={rental}/> : <RentalDetailInfo rental={rental}/>
     }
 
     render() {
@@ -33,7 +39,7 @@ class RentalDetail extends React.Component {
                     <div className='details-section'>
                         <div className='row'>
                             <div className='col-md-8'>
-                                <RentalDetailInfo rental={rental}/>
+                                {this.renderRentalDetail(rental)}
                             </div>
                             <div className='col-md-4'>
                                 <Booking rental={rental}/>
